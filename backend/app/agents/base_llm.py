3c949375238cbfdafa1050de03a6447a14dfed81
Abdullah Ehsan <abdulehsan24@gmail.com>
2025-07-12 23:54:17 +0500

    error was api

--- backend/app/agents/base_llm.py
+++ backend/app/agents/base_llm.py
@@ -1,11 +1,13 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from app.config import gemini_api_key
 
 # Gemini setup with streaming
 llm = ChatGoogleGenerativeAI(
-    model="gemini-1.5-flash",  # or "gemini-pro"
+    model="gemini-2.0-flash",  # or "gemini-pro"
     temperature=0.2,
-    # streaming=True,
+    streaming=True,
     max_tokens=None,
+    google_api_key=gemini_api_key,
     callbacks=[StreamingStdOutCallbackHandler()]  # Optional: logs chunks to stdout
 )
