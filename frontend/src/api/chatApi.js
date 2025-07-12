0f4c884208ab389fe7167e2730f1b079f44b2e98
Abdullah Ehsan <abdulehsan24@gmail.com>
2025-07-12 23:11:27 +0500

    updated requirements and api url

--- frontend/src/api/chatApi.js
+++ frontend/src/api/chatApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-
-const BASE_URL = "http://localhost:8000/chat";
+//"http://localhost:8000/chat" | 
+const BASE_URL = "https://edusync-1iju.onrender.com/chat";
 
 export const sendMessage = async (message) => {
     return axios.post(BASE_URL, { message });
